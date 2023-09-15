ece9741b852d18680eaf8de14cb24c579a8d3ecb
sam <sam.leider@hotmail.com>
2023-09-15 00:51:39 -0400

    First commit

---
+++ Merge/MergeTag.cs
@@ -0,0 +1,116 @@
+﻿using System.Text;
+
+namespace Merge;
+/// <summary>
+/// Represents a utility class for merging tags within a string.
+/// </summary>
+public class MergeTag
+{
+    private readonly string openingTag;
+    private readonly string closingTag;
+    private readonly Dictionary<string, object> tags;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MergeTag"/> class with default opening and closing tags.
+    /// </summary>
+    /// <param name="tags">The dictionary of tags and their associated values.</param>
+    public MergeTag(Dictionary<string, object> tags) : this(tags, "<", ">") { }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MergeTag"/> class with custom opening and closing tags.
+    /// </summary>
+    /// <param name="tags">The dictionary of tags and their associated values.</param>
+    /// <param name="openingTag">The opening tag.</param>
+    /// <param name="closingTag">The closing tag.</param>
+    public MergeTag(Dictionary<string, object> tags, string openingTag, string closingTag)
+    {
+        ValidateTags(openingTag, closingTag);
+        this.tags = tags;
+        this.openingTag = openingTag;
+        this.closingTag = closingTag;
+    }
+
+    /// <summary>
+    /// Validates the opening and closing tags.
+    /// </summary>
+    /// <param name="openingTag">The opening tag.</param>
+    /// <param name="closingTag">The closing tag.</param>
+    private void ValidateTags(string openingTag, string closingTag)
+    {
+        if (string.IsNullOrEmpty(openingTag) || string.IsNullOrEmpty(closingTag))
+        {
+            throw new ArgumentNullException("Tags cannot be empty or null");
+        }
+
+        if (openingTag.Length != closingTag.Length)
+        {
+            throw new ArgumentException("Opening and closing tags should be the same length");
+        }
+
+        if (openingTag == closingTag)
+        {
+            throw new ArgumentException("Opening and closing tags should not be the same");
+        }
+    }
+
+    /// <summary>
+    /// Merges the tags within the given input string.
+    /// </summary>
+    /// <param name="input">The input string containing tags to be merged.</param>
+    /// <returns>The merged string.</returns>
+    public string Merge(string input)
+    {
+        StringBuilder result = new StringBuilder();
+        StringBuilder keyBuilder = new StringBuilder();
+        bool isBuildingKey = false;
+
+        for (int i = 0; i < input.Length; i++)
+        {
+            string currentSubstring = input.Substring(i, Math.Min(openingTag.Length, input.Length - i));
+
+            if (currentSubstring == openingTag)
+            {
+                if (isBuildingKey)
+                {
+                    result.Append(input[i]);
+                    continue;
+                }
+
+                isBuildingKey = true;
+                i += openingTag.Length - 1;
+            }
+            else if (currentSubstring == closingTag)
+            {
+                if (!isBuildingKey)
+                {
+                    result.Append(input[i]);
+                    continue;
+                }
+
+                string key = keyBuilder.ToString();
+                if (!tags.ContainsKey(key))
+                {
+                    throw new ArgumentException($"Cannot find tag for {key} in provided dictionary");
+                }
+
+                result.Append(tags[key].ToString());
+                keyBuilder.Clear();
+                isBuildingKey = false;
+                i += closingTag.Length - 1;
+            }
+            else
+            {
+                if (isBuildingKey)
+                {
+                    keyBuilder.Append(input[i]);
+                }
+                else
+                {
+                    result.Append(input[i]);
+                }
+            }
+        }
+
+        return result.ToString();
+    }
+}
---
+++ MergeTests/MergeTagTests.cs
@@ -0,0 +1,97 @@
+﻿using NUnit.Framework;
+using Merge;
+
+namespace MergeTests
+{
+    [TestFixture]
+    public class MergeTagTests
+    {
+        [Test]
+        public void Merge_WithValidTags_ReturnsMergedString()
+        {
+            var tags = new Dictionary<string, object>
+            {
+                { "name", "John" },
+                { "age", 30 }
+            };
+
+            var mergeTag = new MergeTag(tags);
+            var result = mergeTag.Merge("Hello, <name>. You are <age> years old.");
+
+            Assert.That(result, Is.EqualTo("Hello, John. You are 30 years old."));
+        }
+
+        [Test]
+        public void Merge_WithMissingTag_ThrowsArgumentException()
+        {
+            var tags = new Dictionary<string, object>
+            {
+                { "name", "John" }
+            };
+
+            var mergeTag = new MergeTag(tags);
+
+            Assert.Throws<ArgumentException>(() => mergeTag.Merge("Hello, <name>. You are <age> years old."));
+        }
+
+        [Test]
+        public void Merge_WithCustomTags_ReturnsMergedString()
+        {
+            var tags = new Dictionary<string, object>
+            {
+                { "name", "John" },
+                { "age", 30 }
+            };
+
+            var mergeTag = new MergeTag(tags, "{", "}");
+            var result = mergeTag.Merge("Hello, {name}. You are {age} years old.");
+
+            Assert.That(result, Is.EqualTo("Hello, John. You are 30 years old."));
+        }
+
+        [Test]
+        public void Constructor_WithInvalidOpeningTag_ThrowsArgumentNullException()
+        {
+            var tags = new Dictionary<string, object>
+            {
+                { "name", "John" }
+            };
+
+            Assert.Throws<ArgumentNullException>(() => new MergeTag(tags, null, ">"));
+        }
+
+        [Test]
+        public void Constructor_WithInvalidClosingTag_ThrowsArgumentNullException()
+        {
+            var tags = new Dictionary<string, object>
+            {
+                { "name", "John" }
+            };
+
+            Assert.Throws<ArgumentNullException>(() => new MergeTag(tags, "<", null));
+        }
+
+        [Test]
+        public void Constructor_WithMismatchedTagLength_ThrowsArgumentException()
+        {
+            var tags = new Dictionary<string, object>
+            {
+                { "name", "John" }
+            };
+
+            Assert.Throws<ArgumentException>(() => new MergeTag(tags, "<", ">>"));
+        }
+
+        [Test]
+        public void Constructor_WithIdenticalOpeningAndClosingTags_ThrowsArgumentException()
+        {
+            var tags = new Dictionary<string, object>
+            {
+                { "name", "John" }
+            };
+
+            Assert.Throws<ArgumentException>(() => new MergeTag(tags, "<", "<"));
+        }
+
+    }
+}
